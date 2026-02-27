b1c86358deaf721bbe9711bfb86542dcc66eebd5
mikhail.khvoiko <mikhail.khvoiko.ctr@nthrive.com>
2026-02-27 17:21:54 -0500

    fix for display

--- StarterPlayerScripts/DisplayCaught.lua
+++ StarterPlayerScripts/DisplayCaught.lua
@@ -2,15 +2,15 @@
 	DisplayCaught - Shows a message when you catch a lizard
 	Place in StarterPlayer > StarterPlayerScripts (as a LocalScript)
 	Requires: ReplicatedStorage.LizardCaught RemoteEvent (created by CatchController)
+	CatchController sends: (lizardType, newCount)
 ]]
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
 
-local player = Players.LocalPlayer
 local catchEvent = ReplicatedStorage:WaitForChild("LizardCaught")
 
-catchEvent.OnClientEvent:Connect(function(lizardName, variantName)
-	-- You could show a GUI notification here
-	print("Caught: " .. (variantName or lizardName))
+catchEvent.OnClientEvent:Connect(function(lizardType, newCount)
+	-- Brief feedback - full count is on terrarium banner
+	print("Caught: " .. tostring(lizardType))
 end)
